578614eaa8a816ac51f48609dd869bbadc6b05eb
TAMJID AHMED <ahmed15-4895@diu.edu.bd>
2024-12-18 00:32:54 +0600

    jobapplicationCollection collection created in database

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors=require('cors')
 require('dotenv').config()
 const port =process.env.PORT || 5000
@@ -28,6 +28,42 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+
+const jobscollection=client.db('Job-Portal').collection("jobs")
+const jobapplicationCollection=client.db('Job-Portal').collection("job_applications")
+ app.get('/jobs',async(req,res)=>{
+     // Execute query 
+     const cursor = jobscollection.find();
+     const result =await cursor.toArray();
+     res.send(result)
+
+ })
+ app.get('/jobs/:id',async(req,res)=>{
+    const id=req.params.id
+    const query={_id:new ObjectId(id)}
+    const result=await jobscollection.findOne(query)
+    res.send(result)
+ })
+//  Job Application APi
+app.post('/job-application',async(req,res)=>{
+  const application=req.body
+  const result=await jobapplicationCollection.insertOne(application)
+  res.send(result)
+})
+app.get('/job-application',async(req,res)=>{
+  const application=await jobapplicationCollection.find().toArray();
+  res.json(application)
+
+ 
+})
+
+
+
+
+
+
+
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
